3b11bc366fc270671fd04e8f853e90a7b4d41e01
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-03-03 20:06:28 +0000

    started java script

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+/* This is a MULTI-LINE COMMENT
+Below is an example JS STATEMENT 
+(instruction/command) 
+Every STATEMENT ends with a SEMICOLIN (;)
+console.log is a FUNCTION that displays info 
+*/
+console.log("hello there");
+console.log("general kenobi");
+
+// This is a SINGLE LINE COMMENT 
+
+// DECLARE a new VARIABLE to hold some data 
+let pokemon; 
+
+// ASSIGN a VALUE to that variable 
+// like putting an item in a box 
+pokemon = "pikachu" ; 
+
+// DECLARE + ASSIGN in one statement 
+let name = "matthew"; 
