763d3702604fc99a808ddebb8ba6ae26e2b80323
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-04-07 19:11:30 +0000

    copied stufffffff

--- script.js
+++ script.js
@@ -129,3 +129,31 @@ console.log(result);
 // TRUE  if letter is later in order 
 console.log("Z" > "A"); 
 console.log("Danny" == "Zach");
+
+// Use CONDITIONS in IF STATEMENTS to make DECISIONS about which code to execute 
+let age = 17; 
+// "is the VALUE of the age variable equal to 16?"
+
+if (age == 16) {
+    // code below only runs if the condition above is TRUE 
+    console.log("Happy Sweet Sixteen!") 
+}
+
+else {
+    // code below runs when the IF block is FLASE 
+    console.log("happy birthday!");
+}
+
+// Use ELSE IF blocks to test multiple options 
+age = 15; // change value of age variable 
+if (age == 18) {
+    console.log("Congrats on becoming an adult!");
+}
+
+else if (age > 18) {
+    console.log("Go vote!!"); 
+}
+
+else {
+    console.log("too young to vote..")
+}
